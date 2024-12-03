2cb76348c7478c9014dc06aa795066af1004f02a
Anshul <anshulgoyal454001@gmail.com>
2024-12-03 08:11:11 +0000

    Removed websocket collection constant

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,9 +1,8 @@
 package constants
 
 const (
-	MONGODB_URI          = "mongodb://127.0.0.1:27017"
-	MAIN_DATABASE        = "sashan"
-	USERS_COLLECTION     = "users"
-	POSTS_COLLECTION     = "posts"
-	WEBSOCKER_COLLECTION = "sockets"
+	MONGODB_URI      = "mongodb://127.0.0.1:27017"
+	MAIN_DATABASE    = "sashan"
+	USERS_COLLECTION = "users"
+	POSTS_COLLECTION = "posts"
 )
